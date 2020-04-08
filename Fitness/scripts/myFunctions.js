28d99f519dc90d130061b5436cd55f4939c32cf7
anitagoral <61125675+anitagoral@users.noreply.github.com>
2020-04-08 14:06:19 -0500

    Add files via upload
    A website I created through school readings. The site was designed to learn the basics of learning CSS3 and HTML5.  The book was titled: Website Design with HTML5 and CSS3 by Jessica Minnick.

---
+++ Fitness/scripts/myFunctions.js
@@ -0,0 +1,25 @@
+/*Anita Goral
+02/21/2020
+My Function List
+*/
+
+//Thank you alert
+Function tyAlert() {
+	alert("Thank you for your interest in our fitness club.  We will contact you soon.");
+}
+
+//burpees video window
+Function burpees () {
+	window.open("scripts/burpees.html", "_blank", "width=610, height=360");
+}
+
+//plank video
+Function plank () {
+	window.open("scripts/pank.html","_blank", "width=610 height=360");
+}
+
+//mountain climber video window
+Function mtnClimber () {
+	window.open("scripts/mtn.html","_blank", "width=610 height=360");
+}
+
